18c32784259830167706d84fc1be0fb1d26e940e
timonelafani <timonelafani@gmail.com>
2025-03-25 00:08:55 +0100

    fix: deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.pexels.com"],
+    domains: ["images.pexels.com", "botanist.co.nz"],
   },
 };
 
